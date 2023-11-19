b2e1fffe1e1f33e80148d9ceb6c587c11bd7ff47
FPTSHOP <FPTSHOP@LAPTOP-3CM8UOFV>
2023-11-19 23:02:01 +0700

    lab9

---
+++ lab9/src/task2/ChuongSach.java
@@ -0,0 +1,21 @@
+package task2;
+
+public class ChuongSach {
+	private String title;
+	private int pageCount;
+
+	public ChuongSach(String title, int pageCount) {
+		this.title = title;
+		this.pageCount = pageCount;
+	}
+
+	// Getter methods
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+}
